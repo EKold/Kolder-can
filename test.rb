e783160639f4853305d2b9013c4ed2a5a8c84dc9
EKold <kolderm4@msu.edu>
2020-02-06 16:16:31 -0500

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,12 @@
-ls = "Hello"
+ls = "Hello \n"
 print(ls)
-print("I swear to god if this does not make the desktop appear on another line")
+print("I swear to god if this does not make the desktop appear on another line\n")
+
+i = 1
+
+
+while i < 10
+    puts(i)
+    puts("Mollie is the cutest")
+    i += 1
+end
